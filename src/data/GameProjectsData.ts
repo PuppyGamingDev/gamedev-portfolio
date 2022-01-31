6b424305ce8d83378c9ff3e5660b02ffa6b19338
Shiffed <mike@miketillbrook.co.uk>
2022-01-31 02:19:09 +0000

    update

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -1,7 +1,22 @@
 import ProjectData from '@/data/ProjectData.ts'
 
 export default [
-    new ProjectData("project-1", "Mini Battles", "img/projects/project-1.png", 
+    new ProjectData("project-1", "Simple Crypto WebGL Templates", "img/projects/project-2.png", `
+    <div class="paragraph">
+        <strong>Simple Crypto WebGL Templates</strong> enabled anybody to get NFT data into their game by simply selecting a WebGL template in Player Settings for their preffered blockchain then simply Build! It comes with a demo which give you all the necessary code to get the data you want to reference for each of a users NFTs.
+    </div>
+    <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube.com/embed/gJsexQnvQ6w" frameborder="0" allowfullscreen></iframe>
+    </div>
+
+    <div class="paragraph">
+    <div class="notice">
+        This asset is still awaiting review from the Unity Asset Store.
+    </div>
+    </div>
+
+    `, "#5a78af"),
+    new ProjectData("project-2", "Mini Battles", "img/projects/project-1.png", 
     `
     <div class="paragraph">
      <strong>Mini Battles</strong> is my first game released on any store and serves as an end result for one of my YouTube playlists of combining Super Multipayer Shooter Template and CBS from the Unity Asset Store.
@@ -30,7 +45,7 @@ export default [
         <img class="phone-screenshot" src="https://puppygaming.co.uk/wp-content/uploads/2021/10/Screenshot_20211010-035729.jpg" alt="Mini Battles Screenshot" />
     </div>
     `, "#23bd69", true),
-    new ProjectData("project-2", "YouTube Channel", "img/projects/project-2.png", `
+    new ProjectData("project-3", "YouTube Channel", "img/projects/project-2.png", `
     <div class="paragraph">
         My <strong>YouTube Channel</strong> was created with the aim of helping others by creating guides for the additions or modifications I make with different assets from the Unity Asset Store.
     </div>
